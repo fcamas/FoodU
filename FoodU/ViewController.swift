94c1af1808d1220d186c53460c02a354dd5d6dec
Fredy Camas <fredycamasdev@gmail.com>
2023-08-04 20:08:00 -0400

    initial commit food u

---
+++ FoodU/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodU
+//
+//  Created by Fredy Camas on 6/26/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
