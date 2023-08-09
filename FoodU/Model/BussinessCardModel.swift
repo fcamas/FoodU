57e3058d90f3d514a9fae357a2b6fe750b85d508
Fredy Camas <fredycamasdev@gmail.com>
2023-08-08 20:15:00 -0400

    added business model

---
+++ FoodU/Model/BussinessCardModel.swift
@@ -0,0 +1,23 @@
+//
+//  BussinessCardModel.swift
+//  FoodU
+//
+//  Created by Fredy Camas on 08/06/23.
+//
+
+import Foundation
+
+struct BusinessCardModel{
+    var id:String
+    var name:String
+    var rating:Float
+    var favorite: Bool
+    var price:String
+    var open:Bool
+    var address:[String]
+    var urlImage: String
+    var longitude:Double
+    var latitude:Double
+    
+   
+}
