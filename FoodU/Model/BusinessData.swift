701cea198309e6e44c958ebb2c3dc4c48aaa6b77
Fredy Camas <fredycamasdev@gmail.com>
2023-08-10 20:08:00 -0400

    added business data model

---
+++ FoodU/Model/BusinessData.swift
@@ -0,0 +1,25 @@
+//
+//  BusinessData.swift
+//  FoodU
+//
+//  Created by Fredy Camas on 08/09/23.
+//
+
+import Foundation
+
+struct BusinessData: Codable{
+    
+    var businesses:[Businesses]
+   
+    struct Businesses:Codable {
+        var name: String?
+        var id   : String?
+        var rating  : Float?
+        var price  : String?
+        var is_closed : Bool?
+        var distance : Double?
+        var image_url: String?
+       
+    }
+    
+}
