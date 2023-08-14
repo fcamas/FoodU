f431e539385b49e34d6191cdf694c1e816273bc0
Fredy Camas <fredycamasdev@gmail.com>
2023-08-13 20:32:00 -0400

    added protocol in business managment

---
+++ FoodU/Networking/BusinessManagment.swift
@@ -0,0 +1,13 @@
+//
+//  BusinessNetworking.swift
+//  FoodU
+//
+//  Created by Fredy Camas on 08/10/23.
+//
+
+import UIKit
+import CoreLocation
+
+protocol BusinessManagmentDelegate{
+    func didUpdateBusiness(businessData: [BusinessCardModel])
+}
