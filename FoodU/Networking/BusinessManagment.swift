178c45a2bef205c51b8ef3a27b9410e16fa65b48
Fredy Camas <fredycamasdev@gmail.com>
2023-08-16 20:48:00 -0400

    added networking request func

--- FoodU/Networking/BusinessManagment.swift
+++ FoodU/Networking/BusinessManagment.swift
@@ -28,6 +28,30 @@ struct BusinessManagment {
         let businessURL = "\(baseURL)?latitude=\(latitude)&longitude=\(longitude)&categories=\(category)"
         
         print(businessURL)
-    
+        performRequest(businessURL:businessURL)
+    }
+    func performRequest(businessURL:String){
+       
+        guard  let url = URL(string: businessURL) else {return}
+       var request = URLRequest(url: url)
+        request.setValue("Bearer \(apikey)", forHTTPHeaderField: "Authorization")
+        request.httpMethod = "GET"
+        
+        let session = URLSession(configuration: .default)
+        let task = session.dataTask(with: request) { data, response, error in
+            
+            if error == nil{
+                
+            }
+            
+            if let newData = data {
+                
+               let dataString = String(data: newData, encoding: .utf8)
+                print(dataString)
+               
+            }
+           
+        }
+        task.resume()
     }
 }
