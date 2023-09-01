c596bc244882e46c157b79330efa14d64ecff610
Fredy Camas <fredycamasdev@gmail.com>
2023-08-31 20:56:00 -0400

    added init for search cell

--- FoodU/SearchResultCell.swift
+++ FoodU/SearchResultCell.swift
@@ -16,6 +16,10 @@ class SearchResultCell: UICollectionViewCell {
     weak var delegate: SearchResultCellDelegate?
     
     
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     let imageView: UIImageView = {
         let imageView = UIImageView()
         imageView.contentMode = .scaleAspectFill
