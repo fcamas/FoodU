934df419785423f07a6668d211545f07268921e2
Fredy Camas <fredycamasdev@gmail.com>
2023-09-05 20:43:00 -0400

    added favorite view controller

--- FoodU/FavController.swift
+++ FoodU/FavController.swift
@@ -6,9 +6,20 @@
 //
 
 import UIKit
-class FavController: UICollectionViewController {
+class FavController: UICollectionViewController{
+    
+    init() {
+       let layout = UICollectionViewFlowLayout()
+        super.init(collectionViewLayout: layout)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tabBarItem.badgeValue = "2"
     }
 }
