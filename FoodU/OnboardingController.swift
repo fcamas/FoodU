c476bfc4e9fb9f996ec91745d17797dc835ce489
Fredy Camas <fredycamasdev@gmail.com>
2023-09-25 23:12:00 -0400

    added onboarding controller

---
+++ FoodU/OnboardingController.swift
@@ -0,0 +1,13 @@
+//
+//  Onboarding.swift
+//  FoodU
+//
+//  Created by Fredy Camas on 09/15/23.
+//
+
+import UIKit
+class OnBoardingController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
