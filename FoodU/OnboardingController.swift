fb43771bd249519b5269dbb9c68dd7658e7246a1
Fredy Camas <fredycamasdev@gmail.com>
2023-10-14 02:07:00 -0400

    added core-location on onboarding

--- FoodU/OnboardingController.swift
+++ FoodU/OnboardingController.swift
@@ -6,7 +6,18 @@
 //
 
 import UIKit
+import CoreLocation
 class OnBoardingController: UIViewController {
+    let vc = TabBarView()
+    
+    // MARK: - Properties
+    private let locationManager = CLLocationManager()
+    private var keyboardHeight: CGFloat = 0
+    private let blurEffect = UIBlurEffect(style: .dark)
+    private let blurView = UIVisualEffectView()
+    var longitud:CLLocationDegrees?
+    var latitud:CLLocationDegrees?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
