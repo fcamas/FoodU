1d1cbcb527a5032bf26e559a164e5b11100a09c5
Fredy Camas <fredycamasdev@gmail.com>
2023-11-25 13:40:00 -0500

    fixed tab-bar colors

--- FoodU/TabBarView.swift
+++ FoodU/TabBarView.swift
@@ -7,29 +7,29 @@
 
 import UIKit
 
-class TabBarView: UITabBarController{
+class TabBarView: UITabBarController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         loadTabViews()
     }
+    
     private func loadTabViews() {
+        
         let homeController = HomeController()
         let favController = FavController()
-
+        favController.loadViewIfNeeded()
+        
         homeController.tabBarItem.image = UIImage(systemName: "person")
         let navController = UINavigationController(rootViewController: homeController)
         
-        
         favController.title = "Favorite"
         favController.tabBarItem.image = UIImage(systemName: "star")
-        favController.tabBarItem.badgeValue = "1"
         let navControllerfav = UINavigationController(rootViewController: favController)
         
-        
-        
+
         self.setViewControllers([navController, navControllerfav], animated: false)
-        self.tabBar.backgroundColor = .blue
+        self.tabBar.backgroundColor = .white
     }
 }
