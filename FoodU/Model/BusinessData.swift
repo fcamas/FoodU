ca717b099808595520d636bac16ddd9fc6180ae7
Fredy Camas <fredycamasdev@gmail.com>
2023-11-27 22:41:34 -0500

    added data card model

--- FoodU/Model/BusinessData.swift
+++ FoodU/Model/BusinessData.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct BusinessData: Codable{
+struct Businesses: Codable{
     
     var businesses:[Businesses]
    
--- FoodU/Model/BussinessCardModel.swift
+++ FoodU/Model/BussinessCardModel.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct BusinessCardModel{
+struct BusinessCardModel: Codable,Equatable{
     var id:String
     var name:String
     var rating:Float
@@ -18,8 +18,24 @@ struct BusinessCardModel{
     var urlImage: String
     var longitude:Double
     var latitude:Double
+    static var favoritesKey: String {
+        return "Favorites"
+    }
     
     var addessString: String {
          address.joined(separator: "\n")
+    }
+    
+    init(business: Businesses.Businesses) {
+        self.id = business.id ?? "\(UUID())"
+        self.name = business.name ?? "Coming Soon"
+        self.rating = business.rating ?? 0.0
+        self.favorite = false
+        self.price = business.price ?? "unknown"
+        self.open = business.is_closed  ?? true
+        self.address = business.location?.display_address ?? []
+        self.urlImage = business.image_url ?? ""
+        self.longitude = business.coordinates?.longitude ?? 0.0
+        self.latitude = business.coordinates?.latitude ?? 0.0
     }
 }
